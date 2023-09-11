518e0628e8a96b73b0165b1ec8028c20b29740b4
Miles Monday <miles.monday@slu.edu>
2023-09-11 17:37:30 -0500

    added further implementation of non-square maze

--- src/maze.py
+++ src/maze.py
@@ -8,14 +8,14 @@ class Maze:
         self.graph = Graph(sizen*sizem)
 
         # label the nodes from 0 to (N*N)-1
-        for i in range(0, self.size):
+        for i in range(0, self.sizen):
             self.nodes.append([])
-            for j in range(0, self.size):
-                self.nodes[i].append(i*self.size + j)
+            for j in range(0, self.sizem):
+                self.nodes[i].append(i*self.sizen*self.sizem + j)
 
         # each node in the graph is connected to UP, DOWN, LEFT, RIGHT (if they exist)
-        for i in range(0, self.size):
-            for j in range(0, self.size):
+        for i in range(0, self.sizen):
+            for j in range(0, self.sizem):
                 node = self.nodes[i][j]
                 if i > 0:
                     up = self.nodes[i-1][j]
